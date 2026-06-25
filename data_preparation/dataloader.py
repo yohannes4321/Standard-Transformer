b7d7be3ff05ae38d97e04e5745e89c0182ba7015
Yohannes Alemu <alemuyohannes960@gmail.com>
2026-06-25 15:40:12 +0300

    change

--- data_preparation/dataloader.py
+++ data_preparation/dataloader.py
@@ -8,28 +8,30 @@ from data_preparation.config import encoded_dir, max_len, batch_size
 from data_preparation.dataset import EncodedDataset
 
 
-def get_datasets(stride: int = max_len):
+def get_datasets(train_stride: int = 1, eval_stride: int = None):
     """
     Load train, validation, and test datasets from encoded token ID files
     using sliding-window sequences.
     """
+    if eval_stride is None:
+        eval_stride = max_len
 
     train_dataset = EncodedDataset(
         encoded_dir / "train.pt",
         max_len,
-        stride=stride
+        stride=train_stride
     )
 
     valid_dataset = EncodedDataset(
         encoded_dir / "valid.pt",
         max_len,
-        stride=stride
+        stride=eval_stride
     )
 
     test_dataset = EncodedDataset(
         encoded_dir / "test.pt",
         max_len,
-        stride=stride
+        stride=eval_stride
     )
 
     return train_dataset, valid_dataset, test_dataset
@@ -45,12 +47,12 @@ def get_loaders(
     stride=1:
         fully overlapping sliding windows
 
-    stride=max_len+1:
+    stride=max_len:
         non-overlapping chunks
     """
 
     train_dataset, valid_dataset, test_dataset = get_datasets(
-        stride=stride
+        train_stride=stride, eval_stride=max_len
     )
 
 
